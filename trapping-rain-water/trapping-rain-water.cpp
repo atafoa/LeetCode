7ed1cd86bb9094ca12264ab94d25d856f17b7031
Atafo Abure <atafoa@yahoo.com>
2021-07-26 14:56:18 -0500

    Time: 8 ms (55.86%), Space: 14.5 MB (26.08%) - LeetHub

---
+++ trapping-rain-water/trapping-rain-water.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+       
+        int ans = 0, current = 0;
+        stack<int> st;
+        while(current < height.size())
+        {
+            while(!st.empty() && height[current] > height[st.top()])
+            {
+                int top = st.top();
+                st.pop();
+                if(st.empty())
+                {
+                    break;
+                }
+                int distance = current - st.top() - 1;
+                int bounded_height = min(height[current], height[st.top()]) - height[top];
+                ans += distance * bounded_height;
+            }
+            st.push(current++);
+        }
+        return ans;
+        
+        /* dynamic programming
+        if(height.empty())
+            return 0;
+        int ans = 0;
+        int size = height.size();
+        vector<int> left_max(size),right_max(size);
+        left_max[0] = height[0];
+        for(int i = 1; i < size; i++)
+        {
+            left_max[i] = max(height[i],left_max[i-1]);
+        }
+        right_max[size - 1] = height[size - 1];
+        for(int i = size - 2; i >= 0; i--)
+        {
+            right_max[i] = max(height[i], right_max[i+1]);
+        }
+        for(int i = 1; i < size - 1; i++)
+        {
+            ans+= min(left_max[i], right_max[i] - height[i]);
+        }
+        return ans;
+        */
+        
+    }
+};
