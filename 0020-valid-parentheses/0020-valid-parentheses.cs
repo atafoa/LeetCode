54cc4bb60567a126b5e6b65e8a14b0935c153286
Atafo Abure <atafoa@yahoo.com>
2023-09-13 01:22:58 -0400

    Time: 64 ms (82.51%), Space: 38.3 MB (22.66%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cs
+++ 0020-valid-parentheses/0020-valid-parentheses.cs
@@ -1,5 +1,5 @@
 public class Solution {
-    public bool IsValid(string s) {
+     public bool IsValid(string s) {
         /*
             Only two bad cases
             1. ), or )) or {)
@@ -7,6 +7,16 @@ public class Solution {
             2. (()
             Extra opening paren
         */
+
+
+        //Create a dict to store map of brackets
+        //Create a stack to store open brackets as encountered
+        //Iterate over each char in string
+        // if a char is an open bracket, push it onto the stack
+        // if character is a closing bracket pop the top element from the stack and compare it with the corresponding closing bracket in the dict.
+        // if the popped bracket and the corresponding bracket in the dict do not match false
+        // if stack is empt at end of loop return true
+
         Dictionary<char, char> paren = new();
         paren.Add(')', '(');
         paren.Add('}', '{');
@@ -24,7 +34,7 @@ public class Solution {
                 }
                 stack.Pop();
             }
-            else // c is closing paren
+            else // c is opening paren
             {
                 stack.Push(c);
             }
