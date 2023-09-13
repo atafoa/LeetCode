be203ea9d96bd66c29f6a826e6b570ba036af11c
Atafo Abure <atafoa@yahoo.com>
2023-09-12 22:12:14 -0400

    Time: 62 ms (82.02%), Space: 39.8 MB (73.34%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cs
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cs
@@ -1,5 +1,17 @@
 public class Solution {
     public int LengthOfLongestSubstring(string s) {
+
+        // use a set to keep track of unique characters
+        // maintain two pointers left and right to represent bounds of current substring
+        // The maxlength is the length of the longest substr encountered so far
+        // iterate through the string
+        // if the current char is not in set it means we have a unique char
+        // insert the char into the set and update max length
+        // if already present means char is repeating
+        //      in this case move left pointer forward removing chars from the set until the repeating char gone
+        // return maxLength
+
+        
         int n = s.Length;
         HashSet<Char> set = new();
         int ans = 0, i = 0, j = 0;
