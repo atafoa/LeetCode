288c74b90f7e2be7bf56970f769dec97815095c1
Atafo Abure <atafoa@yahoo.com>
2023-09-17 17:17:43 -0400

    Time: 71 ms (92.07%), Space: 39.2 MB (48.31%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cs
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cs
@@ -1,41 +1,42 @@
 public class Solution {
     public int Search(int[] nums, int target) {
         // Binary Search
-        int low = 0, high = nums.Length - 1;
-        // find the index of the smallest value using binary search
-        // Loop will terminate since mid < hi, and lo or hi will shrink by at least 1.
-        // Proof by contradiction that mid < hi: if mid==hi, then lo==hi and loop would have been terminated.
-        while ( low < high )
-        {
-            int mid = ( low + high ) / 2;
-            if ( nums[mid] > nums[high])
-            {
-                low = mid + 1;
-            }
-            else
-            {
-                high = mid;
-            }
-        }
+        int left = 0; 
+        int right = nums.Length - 1;
 
-        // low = high is the index of the smallest value and also the number of places rotated
-        int rotated = low;
-        low = 0; 
-        high = nums.Length - 1;
-
-        // binary search accouting for rotation
-        while ( low <= high )
+        // first case
+        while (left <= right)
         {
-            int mid = ( low + high ) / 2;
-            int realMid = ( mid + rotated ) % nums.Length;
-            if(nums[realMid] == target)
-                return realMid;
-            if(nums[realMid] < target)
-                low = mid + 1;
-            else 
-                high = mid - 1;
-        }
-        
+            int mid = ( left + right ) / 2;
+            if (target == nums[mid])
+            {
+                return mid;
+            }
+
+            // left sorted portion
+            if(nums[left] <= nums[mid])
+            {
+                if(target > nums[mid] || target < nums[left])
+                {
+                    left = mid + 1; //search right portion
+                }
+                else //target less that mid but greater than left
+                {
+                    right = mid - 1; //search left
+                }
+            }
+            else // right sorted portion
+            {
+                if(target < nums[mid] || target > nums[right])
+                {
+                    right = mid - 1; //search left portion
+                }
+                else //target greater than mid and less than right
+                {
+                    left = mid + 1; //search right
+                }
+            }
+        } 
         return -1;
     }
 }
