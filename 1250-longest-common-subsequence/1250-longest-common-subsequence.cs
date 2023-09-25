ecfa2c83dfe57f85da22faf58e30ce9b5681c04a
Atafo Abure <atafoa@yahoo.com>
2023-09-25 13:29:43 -0400

    Time: 68 ms (72.59%), Space: 40.7 MB (68.64%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.cs
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.cs
@@ -1,20 +1,27 @@
 public class Solution {
     public int LongestCommonSubsequence(string s1, string s2) {
-       int[,] dp = new int[2, s2.Length + 1];
-        for (int i = 1; i <= s1.Length; i++)
+        // create a 2d array of size s1 and s2
+        // iterate through the array and check the characters are equal
+        // if equal add 1 to the previous diagonal value
+        // else take the max of prev row and prev col
+        // return the last value in the array
+
+        int[,] dp = new int[s1.Length+1,s2.Length+1];
+        for(int i = 1; i <= s1.Length; i++)
         {
-            for (int j = 1; j <= s2.Length; j++)
+            for(int j = 1; j <= s2.Length; j++)
             {
-                if (s1[i - 1] == s2[j - 1])
+                if(s1[i-1] == s2[j-1])
                 {
-                    dp[i%2, j] = dp[(i - 1)%2, j - 1] + 1;
+                    dp[i,j] = dp[i-1,j-1]+1;
                 }
                 else
                 {
-                    dp[i%2, j] = Math.Max(dp[(i-1)%2, j], dp[i%2, j - 1]);
+                    dp[i,j] = Math.Max(dp[i-1,j], dp[i,j-1]);
                 }
             }
         }
-        return Math.Max(dp[1, s2.Length],dp[0, s2.Length]);
+
+        return dp[s1.Length, s2.Length];
     }
 }
