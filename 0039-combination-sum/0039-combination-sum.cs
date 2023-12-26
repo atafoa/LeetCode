77fe972d0112ef6ff7eea4d93d8fdeeeef8e8d0b
Atafo Abure <atafoa@yahoo.com>
2023-12-26 13:23:12 -0500

    Time: 110 ms (97.47%), Space: 46.7 MB (20.21%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cs
+++ 0039-combination-sum/0039-combination-sum.cs
@@ -1,48 +1,40 @@
 /*
-    Since the given candidates are unique, if we can simply sort the array we can iterate over the same to find all combinations of elements which forms the sum equals to target.
-
-"Sorting" is only used to make the finding of combination efficient, where we can ignore the further elements if the current sum itself is greater than the target. So one can solve this withoug sorting also.
-
-Also, we need to consider that the sum can be formed using the same element multiple times.
-
-We use backtracking to solve this, where we'll also recursively perform the operations on same index position too for considering the same element multiple times.
+DFS with backtracking
 */
 
 
 public class Solution {
     public IList<IList<int>> CombinationSum(int[] candidates, int target) {
-        IList<IList<int>> result = new List<IList<int>>();
-        
-        //Sorting is to reduce the number of recursions
+          var n = candidates.Length;
         Array.Sort(candidates);
-        BackTrackCombiSum(candidates, target, 0, new List<int>(), result);
+        var result = new List<IList<int>>();
+        var combination = new List<int>(); 
+        Dfs(0, 0);
         
         return result;
-    }
-    
-    private void BackTrackCombiSum(int[] candidates, int target, int idx
-        , List<int> temp, IList<IList<int>> result)
-    {
-        if (target == 0)
-        {
-            result.Add(temp.ToList());
-            return;
-        }
-        else
-        {
-            while (idx < candidates.Length)
+        
+        void Dfs(int startIdx, int sum)
+        {            
+            for(int i = startIdx; i < n; i++)
             {
-                if (candidates[idx] > target)
+                sum += candidates[i];
+                combination.Add(candidates[i]);
+                if (sum < target)
                 {
-                    break;
+                    Dfs(i, sum);
                 }
-
-                temp.Add(candidates[idx]);
-                BackTrackCombiSum(candidates, target - candidates[idx], idx, temp, result);
-                temp.RemoveAt(temp.Count - 1);
-
-                idx++;
+                else if (sum == target)
+                {
+                    result.Add(new List<int>(combination));
+                }
+                else 
+                {
+                    combination.Remove(candidates[i]);
+                    break; // to skip candidates that are also greater
+                }                               
+                combination.Remove(candidates[i]);
+                sum -= candidates[i];                
             }
-        }
+        }        
     }
 }
