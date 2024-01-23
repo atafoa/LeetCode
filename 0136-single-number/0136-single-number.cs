0e4c4d5d86cd6a792eb9fc750c9fd5ad7175e541
Atafo Abure <atafoa@yahoo.com>
2024-01-22 22:44:32 -0500

    Time: 98 ms (68.62%), Space: 45.5 MB (34.66%) - LeetHub

---
+++ 0136-single-number/0136-single-number.cs
@@ -0,0 +1,10 @@
+public class Solution {
+    public int SingleNumber(int[] nums) {
+      int res = 0;
+        foreach(int n in nums)
+        {
+            res = n ^ res;
+        }
+        return res;
+    }
+}
